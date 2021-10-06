dd56daffa44927d523593697e742bb5b2c5088bf
Konstantin <kirilenkokonstantin2003@gmail.com>
2021-10-06 22:58:43 +0400

    secondtask

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
 
-    double pi, a, s, b;
-
-    pi = acos(-1);
-    cin >> a >> b;
-    s = cos(b / pi * 180);
-    cout << a * a * cos((45. / 180) * pi) * sin((45. / 180) * pi) / 2;
+    double x, y, x0, y0, r,s;
+    cin >> x >> y >> x0 >> y0 >> r;
+    s=sqrt(pow(x - x0, 2) + pow(y - y0, 2)) - r;
+    if (s > 0) {
+        cout << s;
+    }
+    else {
+        cout << 0;
+    }
 }
