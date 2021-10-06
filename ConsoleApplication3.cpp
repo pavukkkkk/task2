56ad53476ab0466bc3868969cd6ec5fb80906c9b
Konstantin <kirilenkokonstantin2003@gmail.com>
2021-10-06 22:32:59 +0400

    task1

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,16 +1,13 @@
 ﻿#include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
 
-    double b=0;
-    double pi,a,s;
+    double pi, a, s, b;
 
     pi = acos(-1);;
-    cin >> a, b;
-    s = cos(b / pi * 180) ;
-    cout << a*a*cos((45. / 180) * pi)* sin((45. / 180) * pi)/2;
+    cin >> a >> b;
+    s = cos(b / pi * 180);
+    cout << a * a * cos((45. / 180) * pi) * sin((45. / 180) * pi) / 2;
 }
-
